064ebbc4f41220e53491fdd8aa0038c7fe464fdc
inaraahmadova <tu7r5hs82@code.edu.az>
2024-03-27 22:08:07 +0400

    second commit

---
+++ task10/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10
+{
+
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool isGraduated;
+
+        public Student(string name, string surname, string group, int point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            isGraduated = point >= 65;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"{Name} {Surname} {Group} {Point}bal");
+        }
+        public void CheckGraduated()
+        {
+            if (isGraduated)
+            {
+                Console.WriteLine($"{Name} mezun olub.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} mezun olmuyub.");
+            }
+        }
+        public void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("diplom yoxdur.");
+            }
+            else if (Point >= 65 && Point < 80)
+            {
+                Console.WriteLine("adi diplom.");
+            }
+            else if (Point >= 80 && Point < 90)
+            {
+                Console.WriteLine("sheref diplomu.");
+            }
+            else
+            {
+                Console.WriteLine("yuksek sheref diplomu.");
+            }
+        }
+    }
+}
--- task10/obj/Debug/net6.0/task10.AssemblyInfo.cs
+++ task10/obj/Debug/net6.0/task10.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9431245a17e0c220af995e27002a9f86622028a")]
 [assembly: System.Reflection.AssemblyProductAttribute("task10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
